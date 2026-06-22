fc83abc861757314195f84d7361d38ad9d6dbf20
QuanPVT <anquan0298@gmail.com>
2026-06-22 09:40:32 +0700

    Create Chuyen_doi_string_sang_int.cpp

---
+++ Chuyen_doi_string_sang_int.cpp
@@ -0,0 +1,12 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n = 123;
+    string s = to_string(n);
+    cout << "Type string: " << s << endl;
+    n = stoi(s);
+    cout << "Type INT: " << n << endl;
+    set<int>a;
+    return 0;
+}
