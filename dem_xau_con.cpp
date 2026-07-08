f2b70b7480f2eb8938695a971b7f04d7db92b53e
QuanPVT <anquan0298@gmail.com>
2026-07-08 10:58:27 +0700

    Create dem_xau_con.cpp

---
+++ dem_xau_con.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        int k;
+        cin >> s >> k;
+        int count = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            map<char, int> ans;
+            for (int j = i; j < s.size(); j++)
+            {
+                ans[s[j]]++;
+                if (ans.size() == k)
+                    count++;
+                if (ans.size() > k)
+                    break;
+            }
+        }
+        cout << count << endl;
+    }
+}
